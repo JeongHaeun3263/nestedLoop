ae8e89c16ac0e9e73339581f1f00ce51607dded0
HAEUN JEONG <jhe3263@gmail.com>
2019-07-18 09:00:11 -0400

    Using nested loop

--- nestedLoop/Program.cs
+++ nestedLoop/Program.cs
@@ -10,6 +10,15 @@ namespace nestedLoop
     {
         static void Main(string[] args)
         {
+            for (int i = 2; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    //Console.WriteLine($"{i} * {j} = {i * j}");
+                    Console.WriteLine("{0} * {1} = {2}", i, j, i*j);
+                }
+            }
+
         }
     }
 }
